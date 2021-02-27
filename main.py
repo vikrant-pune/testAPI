6f4d026e5f4d07033672b5739f0d7a94bf76ed53
Vikrant Pawar <vicky.pawar@gmail.com>
2021-02-27 12:53:49 +0530

    Http Exception

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi.responses import HTMLResponse
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from typing import List
 from fastapi import FastAPI, Form, Body
 
@@ -15,7 +15,9 @@ async def login(username: str = Body(...), password: str = Body(...)):
 
 
 @app.post("/files/")
-async def create_files(files: List [bytes] = File(...)):
+async def create_files(files: List[bytes] = File(...)):
+    """ Has a byte opbjects
+    """
     # with open(files, 'r') as file_obj:
     #     data = file_obj.read()
 
@@ -28,7 +30,8 @@ async def create_files(files: List [bytes] = File(...)):
 
 @app.post("/uploadfiles/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
-    
+    """ has File object
+    """
     return {"filenames": [file.filename for file in files],
             "data": [file for file in files ]
     }
@@ -49,3 +52,13 @@ async def main():
 </body>
     """
     return HTMLResponse(content=content)
+
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail={"disMsg": "Search somewhere else"})
+    return {"item": items[item_id]}
