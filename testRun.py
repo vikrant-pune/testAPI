fcadc8e5d2f08f687bf9e22b2aacfa69eccb143c
Vikrant Pawar <vicky.pawar@gmail.com>
2021-03-01 09:30:59 -0100

    generator

---
+++ testRun.py
@@ -0,0 +1,15 @@
+
+
+
+def get_ans( d: int):
+    try:
+        d = d+1
+        yield d
+    finally:
+        print(f"Final value {d}")
+
+if __name__ == '__main__':
+    for x in get_ans(7):
+        print (x)
+    print("foo")
+
